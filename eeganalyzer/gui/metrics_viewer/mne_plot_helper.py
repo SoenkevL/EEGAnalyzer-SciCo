e59b767af48a540ce973528feae42e4d499df34a
Soenke van Loh <soenke.v.l@web.de>
2025-04-29 12:18:35 +0200

    licensing

--- eeganalyzer/gui/metrics_viewer/mne_plot_helper.py
+++ eeganalyzer/gui/metrics_viewer/mne_plot_helper.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 """
+Copyright (C) <2025>  <Soenke van Loh>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
 Helper script to display MNE plots in a separate process.
 
 This script is called by the main application to display MNE plots without blocking
